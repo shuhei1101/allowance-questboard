0548bf94cd29f24ac748367403ceb4a9c5234b59
nishikawa_private <private@example.com>
2024-11-28 04:13:56 +0900

    サービスクラスをモック化 メンバーを取得できなかった時にエラー画面へ pageクラスのメンバーをプライベートに メンバーリポジトリを削除

--- lib/application/member/exception/not_found_members.dart
+++ lib/application/member/exception/not_found_members.dart
@@ -1,5 +1,5 @@
-class NotFoundMembers implements Exception {
-  NotFoundMembers([this.message = "Not found members."]);
+class NotFoundFamily implements Exception {
+  NotFoundFamily([this.message = "Not found members."]);
   final String message;
   @override
   String toString() => "NotFoundMembers Exception: $message";
--- lib/infrastructure/inmemory/im_member_repository.dart
+++ lib/infrastructure/inmemory/im_member_repository.dart
@@ -14,19 +14,20 @@ import '../../domain/shared/money.dart';
 class InMemoryMemberRepository implements MemberRepository {
   @override
   Member? find(MemberId memberId) {
-    return Member(
-      id: MemberId("123456"),
-      familyId: FamilyId("123456"),
-      name: MemberName("Alice"),
-      icon: const Icon(Icons.person),
-      birthday: Birthday(DateTime(2024, 1, 1)),
-      grade: Grade(Education.elementary, 2),
-      exp: MemberExp(11),
-      balance: Money(10),
-      minSavings: Money(10),
-      createdAt: DateTime.now(),
-      updatedAt: DateTime.now(),
-    );
+    return null;
+    // Member(
+    //   id: MemberId("123456"),
+    //   familyId: FamilyId("123456"),
+    //   name: MemberName("Alice"),
+    //   icon: const Icon(Icons.person),
+    //   birthday: Birthday(DateTime(2024, 1, 1)),
+    //   grade: Grade(Education.elementary, 2),
+    //   exp: MemberExp(11),
+    //   balance: Money(10),
+    //   minSavings: Money(10),
+    //   createdAt: DateTime.now(),
+    //   updatedAt: DateTime.now(),
+    // );
   }
 
   @override
