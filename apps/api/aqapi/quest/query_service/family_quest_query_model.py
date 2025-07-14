b0dc1c33d7c890b59cdf7c012320bc9770259d6c
Shuhei4697 <131288584+shuhei1101@users.noreply.github.com>
2025-07-14 12:52:26 +0900

    family_quest_query_model.py を更新

--- apps/api/aqapi/quest/query_service/family_quest_query_model.py
+++ apps/api/aqapi/quest/query_service/family_quest_query_model.py
@@ -17,6 +17,14 @@ class QuestMemberSummary:
             child_id=row.child_id,
             child_icon_id=row.child_icon_id
         )
+        
+@dataclass(frozen=True)
+class QuestMemberSummaries:
+    items: List[QuestMemberSummary]
+
+    @classmethod
+    def from_rows(cls, rows: List[Row]) -> "QuestMemberSummaries":
+        return cls([QuestMemberSummary.from_row(row) for row in rows])
 
 @dataclass(frozen=True)
 class FamilyQuestSummary:
@@ -26,7 +34,7 @@ class FamilyQuestSummary:
     icon_id: int
     is_shared: bool
     is_public: Optional[bool]
-    members: List[QuestMemberSummary]
+    members: QuestMemberSummaries
 
     @classmethod
     def from_group(cls, row: Row, members: List[QuestMemberSummary]) -> "FamilyQuestSummary":
@@ -45,19 +53,21 @@ class FamilyQuestSummaries:
     items: List[FamilyQuestSummary]
 
     @classmethod
-    def from_rows(cls, rows: List[Row]) -> "FamilyQuestSummarys":
-        grouped: Dict[int, tuple[Row, List[QuestMemberSummary]]] = {}
+    def from_rows(cls, rows: List[Row]) -> "FamilyQuestSummaries":
+        grouped: Dict[int, tuple[Row, List[Row]]] = {}
 
         for row in rows:
             quest_id = row.quest_id
-
             if quest_id not in grouped:
                 grouped[quest_id] = (row, [])
-            grouped[quest_id][1].append(QuestMemberSummary.from_row(row))
+            grouped[quest_id][1].append(row)
 
         summaries = [
-            FamilyQuestSummary.from_group(row, members)
-            for row, members in grouped.values()
+            FamilyQuestSummary.from_group(
+                row,
+                QuestMemberSummaries.from_rows(member_rows)
+            )
+            for row, member_rows in grouped.values()
         ]
 
         return cls(items=summaries)
