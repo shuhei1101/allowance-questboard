30766d2ae978625c6999eea0a8f967d5bb3c6f6b
shuhei <shuhei.nishikawa_111@icloud.com>
2025-08-18 00:57:46 +0900

    a

--- packages/backend/src/features/icon-category/value-objects/iconCategoryNames.ts
+++ packages/backend/src/features/icon-category/value-objects/iconCategoryNames.ts
@@ -2,12 +2,12 @@ import { IconCategoryName } from './iconCategoryName';
 import { LanguageId } from '@backend/features/language/value-object/languageId';
 import { IconCategoryTranslationEntity } from '../entity/iconCategoryEntity';
 import { LanguageType } from '@backend/features/language/enum/languageType';
-import { DomainCollection } from '@backend/core/models/domainCollection';
+import { BaseCollection } from '@backend/core/models/baseCollection';
 
 /**
  * アイコンカテゴリ名の値オブジェクト集約
  */
-export class IconCategoryNames extends DomainCollection<IconCategoryName, LanguageId> {
+export class IconCategoryNames extends BaseCollection<IconCategoryName, LanguageId> {
   constructor(items: IconCategoryName[]) {
     super(items);
   }
