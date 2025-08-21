0128581da733f9db6be810a8bcbd07c718a9d4e4
shuhei <shuhei.nishikawa_111@icloud.com>
2025-08-21 17:55:22 +0900

    a

--- packages/frontend/AppNavigator.tsx
+++ packages/frontend/AppNavigator.tsx
@@ -64,7 +64,7 @@ export function AppNavigator(props: AppNavigatorProps) {
       },
     }}>
       <AppStack.Navigator
-        initialRouteName="Auth"
+        initialRouteName="Demo"
         screenOptions={{
           headerShown: false,
         }}
