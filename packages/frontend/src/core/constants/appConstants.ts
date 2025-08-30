f71cd9bee91dd8bd879b7da703560106bbbf6e87
Shuhei4697 <131288584+shuhei1101@users.noreply.github.com>
2025-08-30 15:32:45 +0900

    appConstants.ts を更新

--- packages/frontend/src/core/constants/appConstants.ts
+++ packages/frontend/src/core/constants/appConstants.ts
@@ -12,10 +12,17 @@ class Constants {
   static initialize(): Constants{
     return new Constants(
       iconCategories: IconCategories.fromEmpty(),
-      appIcon: AppIcon
+      appIcon: AppIcon.fromEmpty()
     )
   }
 
+  getAllIcons: () => {
+    return this.iconCategories?.getAllIcons();
+  },
+
+  getAppIcon: (icon: Icon): AppIcon | undefined => {
+    return this.appIcons.get(icon) || undefined;
+  },
 }
 
 export AppConstants = Constants.initialize()
